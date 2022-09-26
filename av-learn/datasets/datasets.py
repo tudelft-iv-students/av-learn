220ac98d231207d622fa4cac0f9530ac37b9dfc7
GulikM <matsvandergulik@gmail.com>
2022-09-26 11:09:11 +0200

    fix type hints (partially)

--- av-learn/datasets/datasets.py
+++ av-learn/datasets/datasets.py
@@ -3,7 +3,6 @@ from functools import reduce
 from typing import Union
 
 from mmcv import Config
-
 from mmdetection3d.mmdet3d.datasets.builder import build_dataset
 from mmdetection3d.mmdet3d.datasets.nuscenes_dataset import \
     NuScenesDataset as MMDET3D_NuScenesDataset
@@ -15,7 +14,7 @@ def _rgetattr(obj, attr, *args):
         return getattr(obj, attr, *args)
     return reduce(_getattr, [obj] + attr.split('.'))
 
-def Dataset(cfg: Union[str, Config], mode: str = "train") -> None:
+def Dataset(cfg: Union[str, Config], mode: str = "train"):
     if isinstance(cfg, str):
         if not os.path.isfile(cfg):
             raise FileNotFoundError(f"config not found: {cfg}")            
@@ -27,7 +26,7 @@ def Dataset(cfg: Union[str, Config], mode: str = "train") -> None:
     mode_cfg = _rgetattr(cfg, f'data.{mode}') # raises AttributeError if cfg does not contain task
     return build_dataset(mode_cfg)
 
-def NuScenesDataset(cfg: Union[str, Config] = r"", mode: str = "train") -> None: # TODO: add default cfg filepath 
+def NuScenesDataset(cfg: Union[str, Config] = r"", mode: str = "train") -> MMDET3D_NuScenesDataset: # TODO: add default cfg filepath 
     dataset = Dataset(cfg, mode)
     if not isinstance(dataset, MMDET3D_NuScenesDataset):
         raise TypeError(f"dataset must be mmdet3d.NuScenesDataset, but got {type(dataset)}")
