49eccb057131bda3ec23f975582118624ff0295c
Thanos Masouris <thanosmas97@gmail.com>
2022-10-10 13:36:37 +0200

    Add config for nuscene's tracking classes

---
+++ avlearn/modules/trackers/kalman_tracker/configs/nuscenes.py
@@ -0,0 +1,10 @@
+# Nuscenes classes for which the tracklets are calculated
+NUSCENES_TRACKING_CLASSES = [
+    'bicycle',
+    'bus',
+    'car',
+    'motorcycle',
+    'pedestrian',
+    'trailer',
+    'truck'
+]
