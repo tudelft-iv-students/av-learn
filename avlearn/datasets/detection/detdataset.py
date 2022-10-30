da5af8238085bc62f598fd202c67b53d3fea6b20
GulikM <matsvandergulik@gmail.com>
2022-10-30 11:01:36 +0000

    handle mmdet or mmdet3d dataset wrappers

--- avlearn/datasets/detection/detdataset.py
+++ avlearn/datasets/detection/detdataset.py
@@ -1,6 +1,6 @@
 import os
 from functools import reduce
-from typing import Any, Callable, Union
+from typing import Any, Callable, List, Union
 
 from mmcv import Config
 from torch.utils.data import Dataset
@@ -41,16 +41,16 @@ class DetectionDataset(Dataset):
         elif not isinstance(cfg, Config):
             raise TypeError('config must be a filename or Config object, '
                             f'but got {type(cfg)}')
+        self.cfg = cfg
 
         if not mode in ['train', 'test', 'val']:
             raise ValueError("mode must be one of 'train', 'test' "
                              f"or 'val', but is '{mode}'")
-        self._mode = mode
 
         mode_cfg = _rgetattr(cfg, f'data.{mode}')  # raises AttributeError
         # if cfg does not contain mode
         self._dataset = build_mmdet3d_dataset(mode_cfg, dict(
-            test_mode=mode == 'val'))  # TODO: test rigorously
+            test_mode=mode == 'test'))
 
         # call setters
         self.past_timesteps = past_timesteps
@@ -65,23 +65,41 @@ class DetectionDataset(Dataset):
         if start < 0 or end > len(self) - 1:
             return []  # index out of range
         return [self._dataset[i] for i in range(start, end)]
-
-    def __getattribute__(self, name: str) -> Any:
+    
+    def format_results(self, results: List[dict], **kwargs):
+        if self._is_dataset_wrapper:
+            return self._dataset.dataset.format_results(results, **kwargs)
+        return self._dataset.format_results(results, **kwargs)
+    
+    @property
+    def _is_dataset_wrapper(self) -> bool:
         """
-        Provides direct access to dependency injected mmdetection3d dataset. 
-        :param name: Attribute name.
+        Checks whether self._dataset is a 
+        mmdet or mmdet3d dataset wrapper.
         """
-        try:
-            return object.__getattribute__(self, name)
-        except AttributeError:
-            try:
-                return object.__getattribute__(self._dataset, name)
-            except AttributeError:
-                raise AttributeError(f"'{type(self)}' and  \
-                    '{type(self._dataset)}'"
-                                     f"objects have no attribute '{name}'")
-
-    # @staticmethod
+        import inspect
+        
+        from mmdet3d_datasets import dataset_wrappers as mmdet3d_dataset_wrappers
+        from mmdet.datasets import dataset_wrappers as mmdet_dataset_wrappers
+        
+        def _get_module_classes(module) -> list:
+            assert inspect.ismodule(module)
+            return [obj for _, obj in inspect.getmembers(module, inspect.isclass) if obj.__module__ == module.__name__]            
+            
+        wrappers = _get_module_classes(mmdet3d_dataset_wrappers)
+        wrappers.extend(_get_module_classes(mmdet_dataset_wrappers))
+        
+        for wrapper in wrappers:
+            if isinstance(self._dataset, wrapper):
+                return True
+        return False
+    
+    @property
+    def test_mode(self) -> bool:
+        if self._is_dataset_wrapper:
+            return self._dataset.dataset.test_mode
+        return self._dataset.test_mode    
+    
     def _validate_timesteps_arg(func: Callable) -> Any:
         def wrapped(self, timesteps: int):
             if not isinstance(timesteps, int):
@@ -97,10 +115,6 @@ class DetectionDataset(Dataset):
     @property
     def dataset(self):
         return self._dataset
-
-    @property
-    def mode(self) -> str:
-        return self._mode
 
     @property
     def past_timesteps(self) -> int:
