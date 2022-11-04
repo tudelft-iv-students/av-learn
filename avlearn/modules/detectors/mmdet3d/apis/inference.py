480b1a0c0bcb69885e877aa402a5e03f2ff5779f
GulikM <matsvandergulik@gmail.com>
2022-11-04 09:47:02 +0000

    Uncomment visualization apis

--- avlearn/modules/detectors/mmdet3d/apis/inference.py
+++ avlearn/modules/detectors/mmdet3d/apis/inference.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-# Removed: All code related to segmentation
+# Removed: All code related to segmentation 
 # Added: Option for init_model to return Config dict alongside model
 
 import re
@@ -60,7 +60,7 @@ def init_model(config, checkpoint=None, device='cuda:0', return_config: bool = F
                         f'but got {type(config)}')
     config.model.pretrained = None
     convert_SyncBN(config.model)
-    config.model.train_cfg = None
+    # config.model.train_cfg = None
     model = build_model(config.model, test_cfg=config.get('test_cfg'))
     if checkpoint is not None:
         checkpoint = load_checkpoint(model, checkpoint, map_location='cpu')
@@ -292,127 +292,127 @@ def init_model(config, checkpoint=None, device='cuda:0', return_config: bool = F
 #         result = model(return_loss=False, rescale=True, **data)
 #     return result, data
 
-# def show_det_result_meshlab(data,
-#                             result,
-#                             out_dir,
-#                             score_thr=0.0,
-#                             show=False,
-#                             snapshot=False):
-#     """Show 3D detection result by meshlab."""
-#     points = data['points'][0][0].cpu().numpy()
-#     pts_filename = data['img_metas'][0][0]['pts_filename']
-#     file_name = osp.split(pts_filename)[-1].split('.')[0]
+def show_det_result_meshlab(data,
+                            result,
+                            out_dir,
+                            score_thr=0.0,
+                            show=False,
+                            snapshot=False):
+    """Show 3D detection result by meshlab."""
+    points = data['points'][0][0].cpu().numpy()
+    pts_filename = data['img_metas'][0][0]['pts_filename']
+    file_name = osp.split(pts_filename)[-1].split('.')[0]
 
-#     if 'pts_bbox' in result[0].keys():
-#         pred_bboxes = result[0]['pts_bbox']['boxes_3d'].tensor.numpy()
-#         pred_scores = result[0]['pts_bbox']['scores_3d'].numpy()
-#     else:
-#         pred_bboxes = result[0]['boxes_3d'].tensor.numpy()
-#         pred_scores = result[0]['scores_3d'].numpy()
+    if 'pts_bbox' in result[0].keys():
+        pred_bboxes = result[0]['pts_bbox']['boxes_3d'].tensor.numpy()
+        pred_scores = result[0]['pts_bbox']['scores_3d'].numpy()
+    else:
+        pred_bboxes = result[0]['boxes_3d'].tensor.numpy()
+        pred_scores = result[0]['scores_3d'].numpy()
 
-#     # filter out low score bboxes for visualization
-#     if score_thr > 0:
-#         inds = pred_scores > score_thr
-#         pred_bboxes = pred_bboxes[inds]
+    # filter out low score bboxes for visualization
+    if score_thr > 0:
+        inds = pred_scores > score_thr
+        pred_bboxes = pred_bboxes[inds]
 
-#     # for now we convert points into depth mode
-#     box_mode = data['img_metas'][0][0]['box_mode_3d']
-#     if box_mode != Box3DMode.DEPTH:
-#         points = Coord3DMode.convert(points, box_mode, Coord3DMode.DEPTH)
-#         show_bboxes = Box3DMode.convert(pred_bboxes, box_mode, Box3DMode.DEPTH)
-#     else:
-#         show_bboxes = deepcopy(pred_bboxes)
+    # for now we convert points into depth mode
+    box_mode = data['img_metas'][0][0]['box_mode_3d']
+    if box_mode != Box3DMode.DEPTH:
+        points = Coord3DMode.convert(points, box_mode, Coord3DMode.DEPTH)
+        show_bboxes = Box3DMode.convert(pred_bboxes, box_mode, Box3DMode.DEPTH)
+    else:
+        show_bboxes = deepcopy(pred_bboxes)
 
-#     show_result(
-#         points,
-#         None,
-#         show_bboxes,
-#         out_dir,
-#         file_name,
-#         show=show,
-#         snapshot=snapshot)
+    show_result(
+        points,
+        None,
+        show_bboxes,
+        out_dir,
+        file_name,
+        show=show,
+        snapshot=snapshot)
 
-#     return file_name
+    return file_name
 
-# def show_proj_det_result_meshlab(data,
-#                                  result,
-#                                  out_dir,
-#                                  score_thr=0.0,
-#                                  show=False,
-#                                  snapshot=False):
-#     """Show result of projecting 3D bbox to 2D image by meshlab."""
-#     assert 'img' in data.keys(), 'image data is not provided for visualization'
+def show_proj_det_result_meshlab(data,
+                                 result,
+                                 out_dir,
+                                 score_thr=0.0,
+                                 show=False,
+                                 snapshot=False):
+    """Show result of projecting 3D bbox to 2D image by meshlab."""
+    assert 'img' in data.keys(), 'image data is not provided for visualization'
 
-#     img_filename = data['img_metas'][0][0]['filename']
-#     file_name = osp.split(img_filename)[-1].split('.')[0]
+    img_filename = data['img_metas'][0][0]['filename']
+    file_name = osp.split(img_filename)[-1].split('.')[0]
 
-#     # read from file because img in data_dict has undergone pipeline transform
-#     img = mmcv.imread(img_filename)
+    # read from file because img in data_dict has undergone pipeline transform
+    img = mmcv.imread(img_filename)
 
-#     if 'pts_bbox' in result[0].keys():
-#         result[0] = result[0]['pts_bbox']
-#     elif 'img_bbox' in result[0].keys():
-#         result[0] = result[0]['img_bbox']
-#     pred_bboxes = result[0]['boxes_3d'].tensor.numpy()
-#     pred_scores = result[0]['scores_3d'].numpy()
+    if 'pts_bbox' in result[0].keys():
+        result[0] = result[0]['pts_bbox']
+    elif 'img_bbox' in result[0].keys():
+        result[0] = result[0]['img_bbox']
+    pred_bboxes = result[0]['boxes_3d'].tensor.numpy()
+    pred_scores = result[0]['scores_3d'].numpy()
 
-#     # filter out low score bboxes for visualization
-#     if score_thr > 0:
-#         inds = pred_scores > score_thr
-#         pred_bboxes = pred_bboxes[inds]
+    # filter out low score bboxes for visualization
+    if score_thr > 0:
+        inds = pred_scores > score_thr
+        pred_bboxes = pred_bboxes[inds]
 
-#     box_mode = data['img_metas'][0][0]['box_mode_3d']
-#     if box_mode == Box3DMode.LIDAR:
-#         if 'lidar2img' not in data['img_metas'][0][0]:
-#             raise NotImplementedError(
-#                 'LiDAR to image transformation matrix is not provided')
+    box_mode = data['img_metas'][0][0]['box_mode_3d']
+    if box_mode == Box3DMode.LIDAR:
+        if 'lidar2img' not in data['img_metas'][0][0]:
+            raise NotImplementedError(
+                'LiDAR to image transformation matrix is not provided')
 
-#         show_bboxes = LiDARInstance3DBoxes(pred_bboxes, origin=(0.5, 0.5, 0))
+        show_bboxes = LiDARInstance3DBoxes(pred_bboxes, origin=(0.5, 0.5, 0))
 
-#         show_multi_modality_result(
-#             img,
-#             None,
-#             show_bboxes,
-#             data['img_metas'][0][0]['lidar2img'],
-#             out_dir,
-#             file_name,
-#             box_mode='lidar',
-#             show=show)
-#     elif box_mode == Box3DMode.DEPTH:
-#         show_bboxes = DepthInstance3DBoxes(pred_bboxes, origin=(0.5, 0.5, 0))
+        show_multi_modality_result(
+            img,
+            None,
+            show_bboxes,
+            data['img_metas'][0][0]['lidar2img'],
+            out_dir,
+            file_name,
+            box_mode='lidar',
+            show=show)
+    elif box_mode == Box3DMode.DEPTH:
+        show_bboxes = DepthInstance3DBoxes(pred_bboxes, origin=(0.5, 0.5, 0))
 
-#         show_multi_modality_result(
-#             img,
-#             None,
-#             show_bboxes,
-#             None,
-#             out_dir,
-#             file_name,
-#             box_mode='depth',
-#             img_metas=data['img_metas'][0][0],
-#             show=show)
-#     elif box_mode == Box3DMode.CAM:
-#         if 'cam2img' not in data['img_metas'][0][0]:
-#             raise NotImplementedError(
-#                 'camera intrinsic matrix is not provided')
+        show_multi_modality_result(
+            img,
+            None,
+            show_bboxes,
+            None,
+            out_dir,
+            file_name,
+            box_mode='depth',
+            img_metas=data['img_metas'][0][0],
+            show=show)
+    elif box_mode == Box3DMode.CAM:
+        if 'cam2img' not in data['img_metas'][0][0]:
+            raise NotImplementedError(
+                'camera intrinsic matrix is not provided')
 
-#         show_bboxes = CameraInstance3DBoxes(
-#             pred_bboxes, box_dim=pred_bboxes.shape[-1], origin=(0.5, 1.0, 0.5))
+        show_bboxes = CameraInstance3DBoxes(
+            pred_bboxes, box_dim=pred_bboxes.shape[-1], origin=(0.5, 1.0, 0.5))
 
-#         show_multi_modality_result(
-#             img,
-#             None,
-#             show_bboxes,
-#             data['img_metas'][0][0]['cam2img'],
-#             out_dir,
-#             file_name,
-#             box_mode='camera',
-#             show=show)
-#     else:
-#         raise NotImplementedError(
-#             f'visualization of {box_mode} bbox is not supported')
+        show_multi_modality_result(
+            img,
+            None,
+            show_bboxes,
+            data['img_metas'][0][0]['cam2img'],
+            out_dir,
+            file_name,
+            box_mode='camera',
+            show=show)
+    else:
+        raise NotImplementedError(
+            f'visualization of {box_mode} bbox is not supported')
 
-#     return file_name
+    return file_name
 
 
 # def show_result_meshlab(data,
