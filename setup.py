a158b8c8c7e45de713d003d301a6705f3b4ddb23
Thanos Masouris <thanosmas97@gmail.com>
2022-12-05 11:31:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="avlearn", version="0.0.1",
     description="An open source framework for end-to-end autonomous vehicle tasks",
     author="AV Learn Contributors",
-    url='https://github.com/ThanosM97/av-learn',
+    url='https://github.com/tudelft-iv-students/av-learn',
     keywords='computer vision, autonomous vehicles',
     license='Apache License 2.0', packages=find_packages())
